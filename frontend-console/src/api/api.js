e8bd4ff0479233d2d1feb017f98330dff11e8c8e
efeeney26 <danilkochkin22@gmail.com>
2019-11-24 15:25:12 +0300

    feat(): add eslint, prop-types; fix(): eslint errors

--- frontend-console/src/api/api.js
+++ frontend-console/src/api/api.js
@@ -1,41 +1,41 @@
 export async function getBooks() {
-	const response = await fetch('/api/books');
-	return await response.json();
+  const response = await fetch('/api/books');
+  return await response.json();
 }
 
 export async function getBook(id) {
-	const response = await fetch(`/api/books/${id}`);
-	return await response.json();
+  const response = await fetch(`/api/books/${id}`);
+  return await response.json();
 }
 
 export async function addBook(bookInfo) {
-	return await fetch('/api/books/add', {
-		method: 'POST',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(bookInfo),
-	});
+  return await fetch('/api/books/add', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(bookInfo),
+  });
 }
 
 export async function updateBook(bookInfo, id) {
-	return await fetch(`/api/books/${id}`, {
-		method: 'PUT',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(bookInfo),
-	})
+  return await fetch(`/api/books/${id}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(bookInfo),
+  });
 }
 
 export async function deleteBook(id) {
-	return await fetch(`/api/books/${id}`, {
-		method: 'DELETE',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		}
-	})
+  return await fetch(`/api/books/${id}`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
 }
--- frontend-console/src/containers/AddBook/AddBook.jsx
+++ frontend-console/src/containers/AddBook/AddBook.jsx
@@ -1,48 +1,60 @@
-import React, {useState} from 'react';
-import {addBook} from "../../api/api";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { addBook } from '../../api/api';
 
 const AddBook = (props) => {
-	const {history} = props;
-	const emptyItem = {
-		name: '',
-		author: '',
-		genre: '',
-		description: ''
-	};
+  const { history } = props;
+  const emptyItem = {
+    name: '',
+    author: '',
+    genre: '',
+    description: '',
+  };
 
-	const [data, setData]  = useState(emptyItem);
+  const [data, setData] = useState(emptyItem);
 
-	const handleChange = (e) => {
-		const {target} = e;
-		const {value, name} = target;
-		let item = {...data};
-		item[name] = value;
-		setData(item);
-	};
+  const handleChange = (e) => {
+    const { target } = e;
+    const { value, name } = target;
+    const item = { ...data };
+    item[name] = value;
+    setData(item);
+  };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		addBook(data)
-			.then(() => {
-				alert('It is working');
-				history.goBack();
-			})
-			.catch(() => alert('SHEEEEEEIT'))
-	};
-	
-	return (
-		<form onSubmit={handleSubmit}>
-			<input type="text" value={data.name} onChange={handleChange} name="name"/>
-			<br/><br/>
-			<input type="text" value={data.author} onChange={handleChange} name="author"/>
-			<br/><br/>
-			<input type="text" value={data.genre} onChange={handleChange} name="genre"/>
-			<br/><br/>
-			<textarea cols={100} rows={10} value={data.description} onChange={handleChange} name="description"/>
-			<br/><br/>
-			<button type="submit">Добавить книгу</button>
-		</form>
-	)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addBook(data)
+      .then(() => {
+        alert('It is working');
+        history.goBack();
+      })
+      .catch(() => alert('SHEEEEEEIT'));
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={data.name} onChange={handleChange} name="name" />
+      <br />
+      <br />
+      <input type="text" value={data.author} onChange={handleChange} name="author" />
+      <br />
+      <br />
+      <input type="text" value={data.genre} onChange={handleChange} name="genre" />
+      <br />
+      <br />
+      <textarea cols={100} rows={10} value={data.description} onChange={handleChange} name="description" />
+      <br />
+      <br />
+      <button type="submit">Добавить книгу</button>
+    </form>
+  );
+};
+
+AddBook.propTypes = {
+  history: PropTypes.shape({
+    goBack: PropTypes.func.isRequired,
+  }).isRequired,
 };
 
 export default AddBook;
